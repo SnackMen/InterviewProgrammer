bc9a6ecd073c227dc5599d23c178c4aa4aa992b8
laowanggit <vipkia@sina.cn>
2016-09-06 15:56:41 +0800

    快指针查找倒数第k个结点

--- Program6/src/com/ws/program/Program.java
+++ Program6/src/com/ws/program/Program.java
@@ -1,6 +1,7 @@
 package com.ws.program;
 
 import linklist.LinkedListNode;
+import sun.awt.image.ImageWatched;
 
 /**
  * Created by laowang on 16-9-6.
@@ -12,7 +13,8 @@ public class Program {
         linkedListNode.appendToTail(2);
         linkedListNode.appendToTail(3);
 
-        nthToLast(linkedListNode,3);
+//        nthToLast(linkedListNode,3);
+        System.out.println(nthToLast1(linkedListNode,3).val);
 
     }
 
@@ -26,5 +28,31 @@ public class Program {
             System.out.println(head.val);
         }
         return i;
+    }
+
+    private  static LinkedListNode nthToLast1(LinkedListNode head,int k){
+        if(k<=0)
+            return null;
+        LinkedListNode p1 = head;
+        LinkedListNode p2 = head;
+
+        //p2向前移动k个结点
+        for(int i=0;i<k-1;i++){
+            if(p2==null)
+                return null;
+            p2=p2.next;
+        }
+        if(p1==null)
+            return null;
+        /**
+         * 现在以同样的速度移动p1和p2,当p2抵达链表末尾时，p1刚好指向倒数的第k个结点
+         */
+
+        while (p2.next!=null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p1;
     }
 }
