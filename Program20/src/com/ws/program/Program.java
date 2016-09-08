f5583f66e3ee1aa1e7fe33670da1d4950866976e
laowanggit <vipkia@sina.cn>
2016-09-08 17:14:22 +0800

    利用先序遍历来，同时将在同一层的数据存在list中

---
+++ Program20/src/com/ws/program/Program.java
@@ -0,0 +1,43 @@
+package com.ws.program;
+
+import com.ws.node.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by laowang on 16-9-8.
+ */
+public class Program {
+
+    private void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level){
+        if(root == null){
+            return;
+        }
+        LinkedList<TreeNode> list = null;
+        if(lists.size() == level){//该层不在链表中
+            list = new LinkedList<TreeNode>();
+            /*
+            以中序遍历所有层组，因此，若这是第一次访问i层，则表示我们已经访问过第0到
+            第i-1层
+            因此，我们可以安全的将这一层加到链表末端
+             */
+            lists.add(list);
+
+        }else
+            list = lists.get(level);
+        list.add(root);
+        createLevelLinkedList(root.left,lists,level+1);
+        createLevelLinkedList(root.right,lists,level+1);
+    }
+
+    ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root){
+        ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
+        createLevelLinkedList(root,lists,0);
+        return lists;
+    }
+
+    public static void main(String []args){
+
+    }
+}
