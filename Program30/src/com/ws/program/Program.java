b1ab7ffdf456619fa51eae98da116f250888b21f
laowanggit <vipkia@sina.cn>
2016-09-12 13:53:28 +0800

    太麻烦，不看了

---
+++ Program30/src/com/ws/program/Program.java
@@ -0,0 +1,15 @@
+package com.ws.program;
+
+/**
+ * Created by laowang on 16-9-12.
+ */
+/*
+给定一个正整数，找出与其二进制表示中1的个数相同的/且
+大小最接近的那个数
+ */
+
+public class Program {
+    public static void main(String []args){
+
+    }
+}
